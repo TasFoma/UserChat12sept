810950958df64459283be427e9eb4ab5b9cd5720
Nastya <93374256+TasFoma@users.noreply.github.com>
2024-10-08 09:00:14 +0700

    Add files via upload

---
+++ UserChat/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using UserChat.Data;
+using MySqlConnector;
+using UserChat;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавление контекста базы данных
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 21)))); // Убедитесь, что у вас правильная версия MySQL
+
+// Регистрация ApiRequest как сервиса
+builder.Services.AddScoped<ApiRequest>();
+
+// Добавление контроллеров с представлениями
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Настройка конвейера обработки запросов
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error"); // Настройка обработки ошибок
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Настройка маршрутизации
+
+app.UseEndpoints(endpoints =>
+{
+    // Настройка маршрутов
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Chat}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+        name: "login",
+        pattern: "Login",
+        defaults: new { controller = "User", action = "Login" });
+
+    endpoints.MapControllerRoute(
+        name: "createUser",
+        pattern: "CreateUser",
+        defaults: new { controller = "User", action = "CreateUser" });
+
+    endpoints.MapControllerRoute(
+        name: "adminChat",
+        pattern: "AdminChat/{uid?}",
+        defaults: new { controller = "AdminChat", action = "Index" });
+});
+app.Run();
