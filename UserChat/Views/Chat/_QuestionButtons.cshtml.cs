93ab8ee7a26157c1b0736ae73f5b1db29e207377
Nastya <93374256+TasFoma@users.noreply.github.com>
2024-10-08 08:59:15 +0700

    Add files via upload

---
+++ UserChat/Views/Chat/_QuestionButtons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UserChat.Views.Chat
+{
+    public class _QuestionButtonsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
